554bcd5e4516dc7ef3bbbd5fb7ceeb5f88c9a2a5
saurabhtiw24 <tiwarisaurabh234@gmail.com>
2025-02-23 01:37:41 +0530

    change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,12 +7,13 @@ function Contact() {
             <h1 className='bg-gradient-to-r from-navcol to-navcoll font-bold text-xl box-content p-4 border-4 text-center md:py-5 tracking-widest'>Contact ME</h1>
             <div className='flex flex-col items-center justify-center mt-10'>
             <form
+            action='post'
              className='bg-slate-200 w-96 px-8 py-6 shadow-xl rounded-xl' name='contactUS' netlify>
                 <h1 className='font-bold text-xl text-center'>Send Your Message</h1>
                 <div className='flex flex-col mt-10 space-y-3'>
                     <label>Full Name</label>
                     <input
-                    className='border rounded-lg shadow appearance-none py-2 px-3 leading-tight focus:outline-none focus:shadow-outline' id="name" name='name' type='text' placeholder='Enter Your Full Name'/>
+                    className='border rounded-lg shadow appearance-none py-2 px-3 leading-tight focus:outline-gitnone focus:shadow-outline' id="name" name='name' type='text' placeholder='Enter Your Full Name'/>
                 </div>
                 <div className='flex flex-col mt-10 space-y-3'>
                     <label>Email Address</label>
